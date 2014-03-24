34c314f2b5aa59143291bd9646a61425a4d894c1
Danny Swaby <danny@swa.by>
2014-03-24 13:11:56 -0500

    that didn't work

---
+++ app/assets/coffee/build.js
@@ -0,0 +1,8 @@
+({
+  baseUrl: ".",
+  name: "./../bower_components/almond/almond",
+  include: "require_main",
+  mainConfigFile: "require_main.js",
+  out: "require_main_built.js",
+  findNestedDependencies: true
+})
