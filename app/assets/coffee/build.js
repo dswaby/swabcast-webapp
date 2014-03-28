f7416188b2e2c09d6d73e7337a3fd4ce7de909c7
Danny Swaby <danny@swa.by>
2014-03-28 18:20:41 -0500

    using almond from bower

--- app/assets/coffee/build.js
+++ app/assets/coffee/build.js
@@ -1,6 +1,6 @@
 ({
   baseUrl: ".",
-  name: "vendor/almond",
+  name: "../bower_components/almond/almond",
   include: "require_main",
   mainConfigFile: "require_main.js",
   out: "require_main_built.js",
