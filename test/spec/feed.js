efd74149a8ee66a9cd845b5b911391ea431f432c
Danny Swaby <danny@swa.by>
2014-03-28 21:14:20 -0500

    adding tests

--- test/spec/feed.js
+++ test/spec/feed.js
@@ -2,17 +2,49 @@
   define(["app", "./../../app/assets/bower_components/chai/chai", "entities/feed"], function(Swabcast, chai) {
     var expect;
     expect = chai.expect;
-    describe("Feeds", function() {
-      describe("Feed Model", function() {
-        it("expect default \"urlRoot\" property to equal \"/feeds\"", function() {
-          var feeds;
-          feeds = new Swabcast.Entities.Feed();
-          expect(feeds.urlRoot).to.equal("feeds");
+    describe("Feed Model", function() {
+      describe("Creation", function() {
+        return it("expect \"/feed\" to have default values", function() {
+          var feed;
+          feed = new Swabcast.Entities.Feed();
+          expect(feed).to.be.ok;
+          expect(feed.urlRoot).to.equal("feeds");
+          expect(feed.get("albumArt")).to.equal("default.jpg");
+        });
+      });
+      describe("Attributes", function() {
+        it("expect can set model Attributes", function() {
+          var feed;
+          feed = new Swabcast.Entities.Feed();
+          expect(feed).to.be.ok;
+          expect(feed.urlRoot).to.equal("feeds");
         });
         return it("expect \"albumArt\" property to have a default image of \"default.jpg\" ", function() {
           var feeds;
           feeds = new Swabcast.Entities.Feed();
+          expect(feeds).to.be.ok;
           expect(feeds.get("albumArt")).to.equal("default.jpg");
+        });
+      });
+      describe("Validation", function() {
+        it("expect \'Invalid\' if the feed does not contain episodes", function() {
+          var feed, fn;
+          feed = new Swabcast.Entities.Feed({
+            "subscriptionTitle": "Test Feed",
+            "albumArt": "test.jpg",
+            "summary": "This is a test feed",
+            "feedUrl": "http://test.com/test.xml",
+            "authors": "Danny Swaby"
+          });
+          feed.on("invalid", function(model, error) {
+            throw Error(model.get("subscriptionTitle") + " " + error);
+          });
+          fn = function() {
+            feed.save({
+              episodes: ""
+            });
+          };
+          return expect(fn).to["throw"]("Test Feed must contain episodes attribute");
         });
       });
     });
